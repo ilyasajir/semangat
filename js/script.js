13aa88833d9bca359ef9b2c15a8fca63c21b007b
ilyasaaz <ilyasajir@gmail.com>
2022-06-07 07:48:25 +0700

    edit credit

--- js/script.js
+++ js/script.js
@@ -1,16 +1,16 @@
 let nama, val;
 const url_string = document.URL;
 const url = new URL(url_string);
-let doi;
+let credit;
 
 if (url.searchParams.get('in') != null) {
-  doi = url.searchParams.get('in');
+  credit = url.searchParams.get('in');
 } else {
-  doi = "Filzah";
+  credit = "Ilyasa";
 }
 
 let footer = document.getElementById("credit");
-footer.innerHTML = doi;
+footer.innerHTML = credit;
 footer.href = "https://www.instagram.com/_ilyasin_/";
 
 function time() {
